331c579d21dada15032c14df148346305dff9767
Riccardo Deidda <127761846+RDbtx@users.noreply.github.com>
2023-05-08 18:54:30 +0200

    Add files via upload

---
+++ src/java/it/unica/dieetech/NewProduct/NewProduct.java
@@ -0,0 +1,103 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package it.unica.dieetech.NewProduct;
+
+import it.unica.dieetech.exceptions.InvalidParamException;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import it.unica.dieetech.utils.Utils;
+
+
+/**
+ *
+ * @author ricle
+ */
+@WebServlet(name = "NewProduct", urlPatterns = {"/NewProduct"})
+public class NewProduct extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        response.setContentType("text/html;charset=UTF-8");
+        
+        String product = request.getParameter("product_p");
+        String quantity = request.getParameter("quantity_p");
+        String software = request.getParameter("software_p");
+        String price = request.getParameter("prezzo_p");
+        String description = request.getParameter("description_p");
+        String webpage = "login.jsp";
+        
+        
+        try{
+            Utils.checkString(product, 1, 20);
+            Utils.checkInteger(quantity, 0, 1000 );
+            Utils.checkFloat(price, 0, 1000000);
+            Utils.checkString(software, 0, 20);
+            Utils.checkString(description, 0, 150);
+            
+            response.sendRedirect("prodottoInserito.jsp");
+        }    
+        catch(InvalidParamException e){
+            
+            request.setAttribute("errorMessage", e.getMessage());
+            request.getRequestDispatcher("error.jsp").forward(request,response);
+            }
+        
+        
+        
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+}
