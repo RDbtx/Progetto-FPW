de5082e5173338deb56876d0da282adce302f9cf
Riccardo Deidda <127761846+RDbtx@users.noreply.github.com>
2023-05-26 00:03:14 +0200

    eliminato print
    eliminato print

--- src/java/it/unica/dieetech/logout/logoutServlet.java
+++ src/java/it/unica/dieetech/logout/logoutServlet.java
@@ -37,20 +37,7 @@ public class logoutServlet extends HttpServlet {
           HttpSession session=request.getSession();  
           session.invalidate();  
           response.sendRedirect("login.jsp");  //effetto il logout e ridirigo sulla pagina di login
-          
-          
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet logoutServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet logoutServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
+         
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
