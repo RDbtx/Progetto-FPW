3b564b2cfaade8ef0d08fb84d54f7452ec5faf1e
Andrea Amato <127879807+andreaamato10@users.noreply.github.com>
2023-05-30 17:53:51 +0200

    Add files via upload

---
+++ js/code.js
@@ -0,0 +1,50 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/JavaScript.js to edit this template
+ */
+
+$(document).ready(function(){
+    var offset = 0;
+    function aggiornaProdotto(prodotto){
+        $('#fotoProdotto').html('<img src="'+ prodotto.foto +'" width="100" alt="Foto' + prodotto.name + '"/>');
+        $("#nomeProdotto").text(prodotto.name);
+        $("#datiProdotto").html("<b>Descrizione</b>: " + prodotto.desc + "<br>"
+                                + "<b>Programmi necessari</b>: " + prodotto.software + "<br>"
+                                + "<b>Prezzo:</b> &#8364 " + prodotto.price + "<br>"
+                                + "<b>Quantità disponibili:</b> " + prodotto.quantity + "<br>"
+                                + "<b>Autore:</b> " + prodotto.user);
+    };
+    $('#prevProduct').click(function(){
+       if(offset > 0)
+           offset--;
+       $.ajax({
+          url:"store",           /* nome servlet */
+          data:{
+              offsetId: offset
+          },
+          dataType:"json",
+          success: function (data, state){
+              aggiornaProdotto(data);
+          },
+          error: function(data, state){}
+       });
+    });
+    $('#nextProduct').click(function(){
+           offset++;
+       $.ajax({
+          url:"store",           /* nome servlet */
+          data:{
+              offsetId: offset
+          },
+          dataType:"json",
+          success: function (data, state){
+              if(data.user === "")
+                  offset--;
+              else
+                aggiornaProdotto(data);
+          },
+          error: function(data, state){}
+       });
+    });
+});
+
